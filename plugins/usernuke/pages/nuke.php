dc3ded76a984692c5b8fb8657b5399999be4eda0
maorninja <maorninja@outlook.com>
2016-10-20 18:41:48 -0700

    Security feature & proper PM nuking
    Makes it so user 1 can never get nuked.

--- plugins/usernuke/pages/nuke.php
+++ plugins/usernuke/pages/nuke.php
@@ -6,6 +6,9 @@ $uid = (int)$_GET["id"];
 
 $user = fetch(query("select * from {users} where id={0}", $uid));
 
+if($uid == 1)
+	kill("You may not nuke the owner.");
+
 if(!$user)
 	kill("User not found");
 
@@ -43,6 +46,10 @@ if(isset($_POST["currpassword"]))
 		//Delete usercomments by user or to user
 		query("delete from {usercomments}
 				where uid={0} or cid={0}", $uid);
+				
+		//Delete the PM's sent to the user or sent by the user
+		query("delete from {pmsgs}
+				where userfrom={0}", $uid);
 
 		//Delete THE USER ITSELF
 		query("delete from {users}
@@ -52,6 +59,9 @@ if(isset($_POST["currpassword"]))
 		query("insert into {ipbans} (ip, reason, date) 
 				values ({0}, {1}, 0)
 				on duplicate key update ip=ip", $user["lastip"], "Nuking ".$user["name"]);
+				
+		//Log that the user is nuked.
+		Report("".$user["name"]."was nuked.");
 				
 		echo "User nuked!<br/>";
 		echo "You will need to ", actionLinkTag("Recalculate statistics now", "recalc");
@@ -76,7 +86,7 @@ echo "
 			<td class=\"cell2\">
 			</td>
 			<td class=\"cell0\">
-				".__("WARNING: This will IP-ban the user, and permanently and irreversibly delete the user itself and all his posts, threads, and profile comments. This user will be gone forever, as if he never existed.")."
+				".__("WARNING: This will IP-ban the user, and permanently and irreversibly delete the user itself and all his posts, threads, private messages, and profile comments. This user will be gone forever, as if he never existed.")."
 				<br/><br/>
 				".__("Please enter your password to confirm.")."
 			</td>
